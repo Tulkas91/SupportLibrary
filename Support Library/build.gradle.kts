62c82719e584a02ed087bb3696d9de948548bf97
Marco Mezzasalma Mac Mini <mmezzasalma@sikuel.it>
2024-10-02 09:37:07 +0200

    Fix bug

--- Support Library/build.gradle.kts
+++ Support Library/build.gradle.kts
@@ -98,7 +98,7 @@ publishing {
 
             groupId = "com.github.Tulkas91" // ID del gruppo, usa il tuo username GitHub
             artifactId = "SupportLibrary" // Nome della libreria/repository su GitHub
-            version = "1.1.4" // Versione della libreria
+            version = "1.1.5" // Versione della libreria
         }
     }
 
