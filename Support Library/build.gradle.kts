2b6989424e2ead68244ac4eda08e2b39ae18080c
Marco Mezzasalma Mac Mini <mmezzasalma@sikuel.it>
2024-10-02 09:06:25 +0200

    Fix bug

--- Support Library/build.gradle.kts
+++ Support Library/build.gradle.kts
@@ -99,7 +99,7 @@ publishing {
 
             groupId = "com.github.Tulkas91" // ID del gruppo, usa il tuo username GitHub
             artifactId = "SupportLibrary" // Nome della libreria/repository su GitHub
-            version = "1.1.2" // Versione della libreria
+            version = "1.1.3" // Versione della libreria
         }
     }
 
--- Support Library/src/main/java/it/mm/supportlibrary/ui/dialog/ReviewDialog.kt
+++ Support Library/src/main/java/it/mm/supportlibrary/ui/dialog/ReviewDialog.kt
@@ -3,6 +3,7 @@ package it.mm.supportlibrary.ui.dialog
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import androidx.fragment.app.FragmentActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.play.core.review.ReviewManagerFactory
 
@@ -32,13 +33,34 @@ class ReviewDialog(val context: Context) : MaterialAlertDialogBuilder(context) {
         request.addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 val reviewInfo = task.result
-                val flow = manager.launchReviewFlow(context as androidx.fragment.app.FragmentActivity, reviewInfo)
+                val flow = manager.launchReviewFlow(context as FragmentActivity, reviewInfo)
                 flow.addOnCompleteListener {
                     // Review completata o chiusa
                 }
             } else {
                 // In caso di errore, apri direttamente la pagina del Play Store
-                val playStoreUrl = "https://play.google.com/store/apps/details?id=TUA_APP_ID"
+
+//                        ReviewManager manager = ReviewManagerFactory.create(context);
+////        ReviewManager manager = new FakeReviewManager(clientActivity);
+//                        Task<ReviewInfo> request = manager.requestReviewFlow();
+//                        request.addOnCompleteListener(google_task -> {
+//                            if (google_task.isSuccessful()) {
+//                                // We can get the ReviewInfo object
+//                                ReviewInfo reviewInfo = google_task.getResult();
+//                                Task<Void> flow = manager.launchReviewFlow(activity, reviewInfo);
+//                                flow.addOnCompleteListener(task -> {
+//                                    // The flow has finished. The API does not indicate whether the user
+//                                    // reviewed or not, or even whether the review dialog was shown. Thus, no
+//                                    // matter the result, we continue our app flow.
+//                                });
+//                            } else {
+//                                // There was some problem, continue regardless of the result.
+//                                // you can show your own rate dialog alert and redirect user to your app page
+//                                // on play store.
+//                            }
+//                        });
+                val appPackageName = context.packageName
+                val playStoreUrl = "https://play.google.com/store/apps/details?id=$appPackageName"
                 context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(playStoreUrl)))
             }
         }
