13e3b9e955a4d2a89c7dca7cde008ce0ccd655d5
Marco Mezzasalma Mac Mini <mmezzasalma@sikuel.it>
2024-10-08 16:42:00 +0200

    Fix bug

--- Support Library/src/main/java/it/mm/supportlibrary/helper/BiometricAuthenticationHelper.kt
+++ Support Library/src/main/java/it/mm/supportlibrary/helper/BiometricAuthenticationHelper.kt
@@ -15,27 +15,28 @@ import java.util.concurrent.Executor
 
 class BiometricAuthenticationHelper(val context: Context) {
 
-    private var executor: Executor? = null
-    private var biometricPrompt: BiometricPrompt? = null
-    private var promptInfo: PromptInfo? = null
+    companion object {
+        private var executor: Executor? = null
+        private var biometricPrompt: BiometricPrompt? = null
+        private var promptInfo: PromptInfo? = null
 
-    fun setPrompt(fragment: Fragment, activity: Activity) {
-        executor = ContextCompat.getMainExecutor(activity)
-        biometricPrompt = BiometricPrompt(
-            fragment,
-            executor!!,
-            object : BiometricPrompt.AuthenticationCallback() {
-                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                    super.onAuthenticationError(errorCode, errString)
-                    //                Toast.makeText(getApplicationContext(),
+        fun setPrompt(fragment: Fragment, activity: Activity) {
+            executor = ContextCompat.getMainExecutor(activity)
+            biometricPrompt = BiometricPrompt(
+                fragment,
+                executor!!,
+                object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                        super.onAuthenticationError(errorCode, errString)
+                        //                Toast.makeText(getApplicationContext(),
 //                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
 //                        .show();
-                }
+                    }
 
-                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                    super.onAuthenticationSucceeded(result)
-                    NotificationCenter.getInstance()
-                        .postNotificationName(NotificationCenter.biometricAuthenticationSuccess)
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+                        NotificationCenter.getInstance()
+                            .postNotificationName(NotificationCenter.biometricAuthenticationSuccess)
 //                    if (KTaripApplication.getInstance().getPrefs().getBoolean("rememberLogin", false)) {
 //                        inputEmail.setText(
 //                            KTaripApplication.getInstance().getPrefs().getString("email", "")
@@ -46,78 +47,80 @@ class BiometricAuthenticationHelper(val context: Context) {
 //                        checkBoxRemember.setChecked(true, true)
 //                        login()
 //                    }
-                }
+                    }
 
-                override fun onAuthenticationFailed() {
-                    super.onAuthenticationFailed()
-                    //                Toast.makeText(getApplicationContext(), "Authentication failed",
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        //                Toast.makeText(getApplicationContext(), "Authentication failed",
 //                        Toast.LENGTH_SHORT)
 //                        .show();
-                }
-            })
-    }
-
-    fun initBiometricPrompt(
-        title: String,
-        subtitle: String,
-        description: String,
-    ) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-            promptInfo = PromptInfo.Builder()
-                .setTitle(title)
-                .setSubtitle(subtitle)
-                .setDescription(description)
-                .setAllowedAuthenticators(BiometricManager.Authenticators.DEVICE_CREDENTIAL or BiometricManager.Authenticators.BIOMETRIC_STRONG)
-                .build()
-        else
-            promptInfo = PromptInfo.Builder()
-                .setTitle(title)
-                .setSubtitle(subtitle)
-                .setDescription(description)
-                .setNegativeButtonText("Annulla")
-                .build()
-    }
-
-    fun showBiometricDialog() {
-        biometricPrompt!!.authenticate(promptInfo!!)
-    }
-
-    fun isBiometricHardWareAvailable(context: Context?): Boolean {
-        var result = false
-        val biometricManager = BiometricManager.from(
-            context!!
-        )
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)) {
-                BiometricManager.BIOMETRIC_SUCCESS -> result = true
-                BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> result = false
-                BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> result = false
-                BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> result = false
-                BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED -> result = true
-                BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED -> result = true
-                BiometricManager.BIOMETRIC_STATUS_UNKNOWN -> result = false
-            }
-        } else {
-            when (biometricManager.canAuthenticate()) {
-                BiometricManager.BIOMETRIC_SUCCESS -> result = true
-                BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> result = false
-                BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> result = false
-                BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> result = false
-            }
+                    }
+                })
         }
-        return result
-    }
 
-    fun deviceHasPasswordPinLock(context: Context): Boolean {
-        val keymgr = context.getSystemService(AppCompatActivity.KEYGUARD_SERVICE) as KeyguardManager
-        if (keymgr.isKeyguardSecure) return true
-        return false
-    }
+        fun initBiometricPrompt(
+            title: String,
+            subtitle: String,
+            description: String,
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+                promptInfo = PromptInfo.Builder()
+                    .setTitle(title)
+                    .setSubtitle(subtitle)
+                    .setDescription(description)
+                    .setAllowedAuthenticators(BiometricManager.Authenticators.DEVICE_CREDENTIAL or BiometricManager.Authenticators.BIOMETRIC_STRONG)
+                    .build()
+            else
+                promptInfo = PromptInfo.Builder()
+                    .setTitle(title)
+                    .setSubtitle(subtitle)
+                    .setDescription(description)
+                    .setNegativeButtonText("Annulla")
+                    .build()
+        }
 
-    fun replace_word(pattern: String): String {
-        val asterisk_val = StringBuilder()
-        for (i in 0 until pattern.length) asterisk_val.append('*')
-        return asterisk_val.toString()
+        fun showBiometricDialog() {
+            biometricPrompt!!.authenticate(promptInfo!!)
+        }
+
+        fun isBiometricHardWareAvailable(context: Context?): Boolean {
+            var result = false
+            val biometricManager = BiometricManager.from(
+                context!!
+            )
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)) {
+                    BiometricManager.BIOMETRIC_SUCCESS -> result = true
+                    BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> result = false
+                    BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> result = false
+                    BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> result = false
+                    BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED -> result = true
+                    BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED -> result = true
+                    BiometricManager.BIOMETRIC_STATUS_UNKNOWN -> result = false
+                }
+            } else {
+                when (biometricManager.canAuthenticate()) {
+                    BiometricManager.BIOMETRIC_SUCCESS -> result = true
+                    BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> result = false
+                    BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> result = false
+                    BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> result = false
+                }
+            }
+            return result
+        }
+
+        fun deviceHasPasswordPinLock(context: Context): Boolean {
+            val keymgr =
+                context.getSystemService(AppCompatActivity.KEYGUARD_SERVICE) as KeyguardManager
+            if (keymgr.isKeyguardSecure) return true
+            return false
+        }
+
+        fun replace_word(pattern: String): String {
+            val asterisk_val = StringBuilder()
+            for (i in 0 until pattern.length) asterisk_val.append('*')
+            return asterisk_val.toString()
+        }
     }
 }
