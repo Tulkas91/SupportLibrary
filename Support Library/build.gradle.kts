cd938fcbcc726287b43997494509e18f93d7c0a9
Marco Mezzasalma Mac Mini <mmezzasalma@sikuel.it>
2024-12-27 10:17:49 +0100

    fix estensione file audio

--- Support Library/build.gradle.kts
+++ Support Library/build.gradle.kts
@@ -110,7 +110,7 @@ publishing {
 
             groupId = "com.github.Tulkas91" // ID del gruppo, usa il tuo username GitHub
             artifactId = "SupportLibrary" // Nome della libreria/repository su GitHub
-            version = "1.4.4" // Versione della libreria
+            version = "1.4.5" // Versione della libreria
         }
     }
 
