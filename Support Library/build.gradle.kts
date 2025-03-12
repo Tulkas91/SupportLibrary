1d4e9a8e29fe03765b7d960f386e4f38d267b986
Marco Mezzasalma Mac Mini <mmezzasalma@sikuel.it>
2025-03-12 11:39:13 +0100

    fix materialDialogFragment 3

--- Support Library/build.gradle.kts
+++ Support Library/build.gradle.kts
@@ -110,7 +110,7 @@ publishing {
 
             groupId = "com.github.Tulkas91" // ID del gruppo, usa il tuo username GitHub
             artifactId = "SupportLibrary" // Nome della libreria/repository su GitHub
-            version = "1.4.10" // Versione della libreria
+            version = "1.4.11" // Versione della libreria
         }
     }
 
