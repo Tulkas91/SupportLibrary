93c104a1f9887f80fab3bd53536bc6de87ddad6f
Marco Mezzasalma Mac Mini <mmezzasalma@sikuel.it>
2025-09-15 17:10:04 +0200

    Fix bug BiometricAuthenticationHelper 3

--- Support Library/build.gradle.kts
+++ Support Library/build.gradle.kts
@@ -113,7 +113,7 @@ publishing {
 
             groupId = "com.github.Tulkas91" // ID del gruppo, usa il tuo username GitHub
             artifactId = "SupportLibrary" // Nome della libreria/repository su GitHub
-            version = "1.4.23" // Versione della libreria
+            version = "1.4.24" // Versione della libreria
         }
     }
 
